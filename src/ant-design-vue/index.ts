d6f48722c03e0bd170f2269698ee23207de7d899
Luckearth <luckearth@luckearth.cn>
2021-03-09 13:56:49 +0800

    完成页面布局

--- src/ant-design-vue/index.ts
+++ src/ant-design-vue/index.ts
@@ -1,10 +1,11 @@
 import { App } from 'Vue'
-import { Layout, Button, Input } from 'ant-design-vue'
+import { Layout, Button, Input,Menu } from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 const components = [
   Layout,
   Button,
-  Input
+  Input,
+  Menu
 ]
 export function setupAntd(app: App) {
   components.forEach(component => app.use(component))
